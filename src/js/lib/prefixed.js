d29d5afaacbb360c9e292d53d2a85986133033c5
Thomas Wilburn <news@thomaswilburn.net>
2016-02-05 16:39:00 -0800

    Added library code, cleaned up washes

---
+++ src/js/lib/prefixed.js
@@ -0,0 +1,28 @@
+var strings = {
+  matches: {
+    on: document.body,
+    props: ["matches", "webkitMatchesSelector", "msMatchesSelector", "matchesSelector"]
+  },
+  requestAnimationFrame: {
+    on: window,
+    props: ["webkitRequestAnimationFrame", "requestAnimationFrame", "setTimeout"]
+  },
+  requestFullScreen: {
+    on: document.body,
+    props: ["webkitRequestFullscreen", "mozRequestFullScreen", "msRequestFullscreen", "requestFullscreen"]
+  }
+};
+
+for (var k in strings) {
+  var sentinel = strings[k].on;
+  var props = strings[k].props;
+  for (var i = 0; i < props.length; i++) {
+    if (props[i] in sentinel) {
+      strings[k] = props[i];
+      break;
+    }
+  }
+  if (typeof strings[k] == "object") strings[k] = null;
+}
+
+module.exports = strings;
---
+++ src/js/wash.js
@@ -0,0 +1,99 @@
+var $ = require("./lib/qsa");
+var closest = require("./lib/closest");
+
+var DROP_COUNT = 5;
+
+var lookup = [];
+var tau = Math.PI * 2;
+var points = 16;
+for (var i = 0; i < points; i++) {
+  lookup.push({
+    cos: Math.cos(tau / points * i),
+    sin: Math.sin(tau / points * i)
+  });
+}
+
+var faders = $(".image-container.wash");
+
+var scrollNotified = [];
+
+var onScroll = function() {
+  scrollNotified = scrollNotified.filter(function(sub) {
+    var bounds = sub.el.getBoundingClientRect();
+    if (bounds.top > 0 && bounds.top < window.innerHeight / 2) {
+      sub.callback();
+      return false;
+    }
+    return true;
+  });
+};
+
+window.addEventListener("scroll", onScroll);
+
+var subscribe = function(el, callback) {
+  scrollNotified.push({ el, callback });
+  onScroll();
+};
+
+faders.forEach(function(fader) {
+
+  var image = fader.querySelector("img.base");
+  var canvas = fader.querySelector("canvas");
+  var context = canvas.getContext("2d");
+
+  var source = new Image();
+  source.src = canvas.getAttribute("data-src");
+
+  source.onload = function() {
+    canvas.width = source.width;
+    canvas.height = source.height;
+
+    var pattern = context.createPattern(source, "no-repeat");
+    context.fillStyle = pattern;
+    context.globalAlpha = 0.1;
+
+    var radius = 0;
+    var drops = [];
+    for (var i = 0; i < DROP_COUNT; i++) {
+      drops.push({
+        x: canvas.width * Math.random(),
+        y: canvas.height * Math.random()
+      })
+    }
+    
+    var splot = function(radius) {
+      drops.forEach(function(point) {
+        context.beginPath();
+        context.moveTo(point.x + radius, point.y)
+        for (var i = 0; i < lookup.length; i++) {
+          var j = 1 - (Math.random() * .2);
+          var r = radius * j;
+          var s = r * lookup[i].sin;
+          var c = r * lookup[i].cos;
+          context.lineTo(point.x + c, point.y + s);
+        }
+        // context.arc(c.x, c.y, radius * (1 - i), 0, Math.PI * 2);
+        context.fill();
+      });
+    };
+
+    var frame = function() {
+      var now = Date.now();
+      var elapsed = now - time;
+      time = now;
+      splot(radius);
+      radius += elapsed * .1;
+      if (radius > canvas.width) return;
+      requestAnimationFrame(frame);
+    };
+
+    var time;
+    subscribe(fader, function() {
+      time = Date.now();
+      frame();
+    });
+  };
+
+});
+
+onScroll();
