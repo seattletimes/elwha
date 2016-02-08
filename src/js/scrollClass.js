c3533dc0483a4f19a7f79f86f7f2b16fcbef52a3
Thomas Wilburn <news@thomaswilburn.net>
2016-02-08 15:58:27 -0800

    Fix scroll class targeting

--- src/js/scrollClass.js
+++ src/js/scrollClass.js
@@ -4,7 +4,7 @@ var debounce = require("./lib/debounce");
 var elements = $("[scroll-class]").map(function(target) {
   var area = target.getAttribute("scroll-trigger");
   if (area) {
-    area = document.querySelector(area);
+    area = document.querySelector(area) || target;
   } else {
     area = target;
   }
@@ -15,7 +15,7 @@ var elements = $("[scroll-class]").map(function(target) {
 window.addEventListener("scroll", debounce(function() {
   elements.forEach(function(scroll) {
     var bounds = scroll.area.getBoundingClientRect();
-    if (bounds.top < window.innerHeight) {
+    if (bounds.top < window.innerHeight * .5) {
       scroll.target.classList.add(scroll.className);
     } else {
       scroll.target.classList.remove(scroll.className);
